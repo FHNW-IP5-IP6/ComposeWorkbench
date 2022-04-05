d2ac5293cf5f19910bed2c1d1650ef6631f7723a
sven boehm <boehm.sv@gmail.com>
2022-04-05 08:30:46 +0200

    Add comments to workbench

--- Demo/demoApps/src/main/kotlin/AddressTool.kt
+++ Demo/demoApps/src/main/kotlin/AddressTool.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.ui.window.application
 
 fun main() {
 
@@ -24,7 +23,6 @@ fun main() {
     }
 
     workbench.requestExplorer<AddressExplorerModel>("AddressExplorer", "Address Explorer", AddressExplorerModel())
-
 
     workbench.run()
 }
--- lib/src/main/kotlin/Workbench.kt
+++ lib/src/main/kotlin/Workbench.kt
@@ -13,6 +13,13 @@ class Workbench {
 
     private val model: WorkbenchModel = WorkbenchModel()
 
+    /**
+     * Add an explorer to the Workbench
+     *
+     * TODO: can this be called after run?
+     * @param title: Display title of this explorer TODO: should this be a composable fun
+     * @param content: Composable function that defines the displayed content of this explorer
+     */
     fun <M> registerExplorer(
         type: String,
         content: @Composable (M) -> Unit,
@@ -25,7 +32,13 @@ class Workbench {
         model.registeredExplorers.put(type, explorer);
     }
 
-
+    /**
+     * Add an editor to the Workbench.
+     *
+     * @param M: Type of the Model which the editor uses to manage and display data
+     * @param type: the type of data this editor can be used for
+     * @param content: Composable function that defines the displayed content of this explorer
+     */
     fun <M> registerEditor(
         type: String,
         content: @Composable (M) -> Unit
@@ -37,7 +50,13 @@ class Workbench {
         model.registeredEditors.put(type, explorer);
     }
 
-
+    /**
+     * Explore given Model with explorer of type
+     *
+     * @param M: Model which the explorer uses to manage and display data
+     * @param type: The type of data which the Explorer is used for
+     * @param title: Display title of the requested editor
+     */
     fun <M> requestExplorer(type: String, title: String, m: M) {
         var explorer = model.registeredExplorers.get(type)
         if(explorer != null){
@@ -48,17 +67,27 @@ class Workbench {
         }
     }
 
-
+    /**
+     * Edit given Model with editor of given type
+     *
+     * @param M: Model which the editor uses to manage and display data
+     * @param type: The type of data which the Editor is used for
+     * @param title: Display title of the requested editor
+     * @param onClose: The callback to be invoked when this editor is closed
+     */
     fun <M> requestEditor(type: String, title: String, m: M, onClose: (M) -> Unit ={}) {
         var editor = model.registeredEditors.get(type)
         if(editor != null){
             editor as WorkbenchModule<M>
-            val t = WorkbenchModuleState<M>(title, m, editor, model::removeTab, DisplayType.TAB, onClose)
+            val t = WorkbenchModuleState<M>(title, m, editor, model::removeTab, DisplayType.WINDOW, onClose)
             model.modules += t
             model.selectedTab = t
         }
     }
 
+    /**
+     * Run the Workbench
+     */
     fun run() = application {
         // init mainwindow
         WorkbenchMainUI(model, ::exitApplication)
