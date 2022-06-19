6f815ef5b98946b160da47dbd5d39f7377868717
Dominik Frei <domi_frei@hotmail.com>
2022-06-19 10:46:47 +0200

    Adjusted font size of application.

--- lib/src/main/kotlin/view/component/WorkbenchAppBar.kt
+++ lib/src/main/kotlin/view/component/WorkbenchAppBar.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,7 +23,7 @@ internal fun WorkbenchAppBar(model: WorkbenchModel) {
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             Text(model.appTitle,
-                fontSize = 30.sp,
+                fontSize = MaterialTheme.typography.h4.fontSize,
                 color = Color.White,
                 modifier = Modifier.align(alignment = Alignment.CenterVertically),
             )
--- lib/src/main/kotlin/view/themes/composeworkbench/Typography.kt
+++ lib/src/main/kotlin/view/themes/composeworkbench/Typography.kt
@@ -34,18 +34,19 @@ private val appFontFamily = FontFamily(
 )
 
 private val defaultTypography = Typography()
+private const val FACTOR = 0.75
 val NotoSansTypography = Typography(
-	h1 = defaultTypography.h1.copy(fontFamily = appFontFamily),
-	h2 = defaultTypography.h2.copy(fontFamily = appFontFamily),
-	h3 = defaultTypography.h3.copy(fontFamily = appFontFamily),
-	h4 = defaultTypography.h4.copy(fontFamily = appFontFamily),
-	h5 = defaultTypography.h5.copy(fontFamily = appFontFamily),
-	h6 = defaultTypography.h6.copy(fontFamily = appFontFamily),
-	subtitle1 = defaultTypography.subtitle1.copy(fontFamily = appFontFamily),
-	subtitle2 = defaultTypography.subtitle2.copy(fontFamily = appFontFamily),
-	body1 = defaultTypography.body1.copy(fontFamily = appFontFamily),
-	body2 = defaultTypography.body2.copy(fontFamily = appFontFamily),
-	button = defaultTypography.button.copy(fontFamily = appFontFamily),
-	caption = defaultTypography.caption.copy(fontFamily = appFontFamily),
-	overline = defaultTypography.overline.copy(fontFamily = appFontFamily)
+	h1 = 		defaultTypography.h1		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h1			.fontSize * FACTOR),
+	h2 = 		defaultTypography.h2		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h2			.fontSize * FACTOR),
+	h3 = 		defaultTypography.h3		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h3   		.fontSize * FACTOR),
+	h4 = 		defaultTypography.h4		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h4   		.fontSize * FACTOR),
+	h5 = 		defaultTypography.h5		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h5   		.fontSize * FACTOR),
+	h6 = 		defaultTypography.h6		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.h6   		.fontSize * FACTOR),
+	subtitle1 = defaultTypography.subtitle1	.copy(fontFamily = appFontFamily, fontSize = defaultTypography.subtitle1   	.fontSize * FACTOR),
+	subtitle2 = defaultTypography.subtitle2	.copy(fontFamily = appFontFamily, fontSize = defaultTypography.subtitle2   	.fontSize * FACTOR),
+	body1 = 	defaultTypography.body1		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.body1   		.fontSize * FACTOR),
+	body2 = 	defaultTypography.body2		.copy(fontFamily = appFontFamily, fontSize = defaultTypography.body2   		.fontSize * FACTOR),
+	button = 	defaultTypography.button	.copy(fontFamily = appFontFamily, fontSize = defaultTypography.button   	.fontSize * FACTOR),
+	caption = 	defaultTypography.caption	.copy(fontFamily = appFontFamily, fontSize = defaultTypography.caption   	.fontSize * FACTOR),
+	overline = 	defaultTypography.overline	.copy(fontFamily = appFontFamily, fontSize = defaultTypography.overline   	.fontSize * FACTOR)
 )
