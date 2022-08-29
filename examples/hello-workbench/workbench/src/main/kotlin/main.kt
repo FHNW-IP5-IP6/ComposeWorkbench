57e4ecb262807e4dc131b4f0d01d25af787687e4
Dominik Frei <domi_frei@hotmail.com>
2022-08-29 17:06:20 +0200

    Moved Explorer view to seprate function.

--- examples/hello-workbench/workbench/src/main/kotlin/main.kt
+++ examples/hello-workbench/workbench/src/main/kotlin/main.kt
@@ -1,59 +1,35 @@
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Palette
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 const val COLORS = "colors"
 const val COLOR = "color"
 
-@OptIn(ExperimentalMaterialApi::class)
 fun main() {
-
-    val colorRepository = mutableListOf<Color>(Color(0xff182260), Color(0xff9C27B0), Color(0xff009688), Color(0xff7986CB), Color(0xff0069e0) , Color(0xff8fd3d8), Color(0xffF76c6c), Color(0xff333333))
+    val colorRepository = mutableListOf(Color(0xff55efc4), Color(0xff74b9ff), Color(0xffa29bfe), Color(0xfffdcb6e), Color(0xffd63031) , Color(0xffdfe6e9), Color(0xff636e72), Color(0xff2d3436))
     val workbench = Workbench("Hello Workbench", true)
 
     workbench.registerExplorer<List<Color>>(
         type = COLORS,
         title = { colors -> "Colors: ${colors.size}" },
-        init = { _, _ -> }
-    ) { colors ->
-        Column {
-            colors.forEachIndexed { i, c ->
-                Card (
-                    modifier = Modifier.padding(5.dp).height(25.dp).fillMaxWidth(),
-                    backgroundColor = c,
-                    onClick = { workbench.requestEditor(
-                        type = COLOR,
-                        id = i
-                    ) }
-                ) { }
-            }
-        }
-    }
-
+        init = { _, _ -> },
+        explorerView = { colors -> RgbExplorerUI(colors) { workbench.requestEditor(type = COLOR, id = it) } }
+    )
     workbench.requestExplorer<List<Color>>(type = COLORS, c = colorRepository, listed = true)
 
     workbench.registerEditor<RgbController>(
-        type =COLOR,
+        type = COLOR,
         title = { it.rgbState.title() },
         initController =  {i, mqtt -> RgbController(i, colorRepository[i]){mqtt.publishUnsaved(COLOR, i)} },
         icon = Icons.Filled.Edit,
         onSave = { c, _ ->
             colorRepository[c.rgbState.index] = Color(c.rgbState.r, c.rgbState.g, c.rgbState.b, 1f)
             success()
-        }
-    ){
-        RgbEditorUi(it)
-    }
+        },
+        editorView = { RgbEditorUi(it) }
+    )
 
     workbench.registerEditor<ColorPickerController>(
         type = COLOR,
@@ -68,11 +44,7 @@ fun main() {
         ColorPickerUi(it)
     }
 
-    workbench.run {
-        println("Exit Hello Workbench")
-        success()
-    }
-
+    workbench.run { println("Exit Workbench"); success() }
 }
 
 
